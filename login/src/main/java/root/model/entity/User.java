b974aa3fc0de58c1279a3c2d29f812d583a94380
Toan040606 <toannickson2k60604@gmail.com>
2026-01-21 02:34:46 +0700

    feat: convert Java model to Hibernate unity using Lombok annotations

---
+++ login/src/main/java/root/model/entity/User.java
@@ -0,0 +1,31 @@
+package root.model.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "Users")
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+}
